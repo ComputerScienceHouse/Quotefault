9b4bee54c75bc07d82dd67818ab071bcbc950292
Nicholas Mercadante <nvm6501@rit.edu>
2017-02-20 21:23:34 -0500

    fixed a syntax error in the env config

--- config.env.py
+++ config.env.py
@@ -6,7 +6,7 @@ IP = os.environ.get('QUOTEFAULT_IP', '127.0.0.1')
 PORT = os.environ.get('QUOTEFAULT_PORT', '8080')
 SERVER_NAME = os.environ.get('QUOTEFAULT_SERVER_NAME', 'quotefault.csh.rit.edu')
 #db info
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///{}'.format(os.path.join(os.getcwd(), "data.db"))
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///{}'.format(os.path.join(os.getcwd(), "data.db")))
 #DB_HOST = os.environ.get('QUOTEFAULT_DB_HOST', 'mysql.csh.rit.edu')
 #DB_USERNAME = os.environ.get('QUOTEFAULT_DB_USERNAME', 'quotefault')
 #DB_NAME = os.environ.get('QUOTEFAULT_DB_NAME', 'quotefault')
