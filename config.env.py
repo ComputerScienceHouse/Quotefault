c4e03b90549342c230a31b129fdc93d551c14f48
Nicholas Mercadante <nvm6501@rit.edu>
2017-02-20 20:44:59 -0500

    updated config env and sample to accurately reflect how the site should be accessing the db

--- config.env.py
+++ config.env.py
@@ -6,10 +6,11 @@ IP = os.environ.get('QUOTEFAULT_IP', '127.0.0.1')
 PORT = os.environ.get('QUOTEFAULT_PORT', '8080')
 SERVER_NAME = os.environ.get('QUOTEFAULT_SERVER_NAME', 'quotefault.csh.rit.edu')
 #db info
-DB_HOST = os.environ.get('QUOTEFAULT_DB_HOST', 'mysql.csh.rit.edu')
-DB_USERNAME = os.environ.get('QUOTEFAULT_DB_USERNAME', 'quotefault')
-DB_NAME = os.environ.get('QUOTEFAULT_DB_NAME', 'quotefault')
-DB_PASSWORD = os.environ.get('QUOTEFAULT_DB_PASSWORD', '')
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///{}'.format(os.path.join(os.getcwd(), "data.db"))
+#DB_HOST = os.environ.get('QUOTEFAULT_DB_HOST', 'mysql.csh.rit.edu')
+#DB_USERNAME = os.environ.get('QUOTEFAULT_DB_USERNAME', 'quotefault')
+#DB_NAME = os.environ.get('QUOTEFAULT_DB_NAME', 'quotefault')
+#DB_PASSWORD = os.environ.get('QUOTEFAULT_DB_PASSWORD', '')
 # OpenID Connect SSO config
 OIDC_ISSUER = os.environ.get('QUOTEFAULT_OIDC_ISSUER', 'https://sso.csh.rit.edu/realms/csh')
 OIDC_CLIENT_CONFIG = {
--- config.sample.py
+++ config.sample.py
@@ -1,13 +1,16 @@
+import os
+
 # Flask config
 DEBUG = False
 IP = '127.0.0.1'
 PORT = '8080'
 SERVER_NAME = 'localhost:8080'
 #db info
-DB_HOST = 'mysql.csh.rit.edu'
-DB_USERNAME = 'quotefault'
-DB_NAME = 'quotefault'
-DB_PASSWORD = ''
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(os.getcwd(), "data.db"))
+#DB_HOST = 'mysql.csh.rit.edu'
+#DB_USERNAME = 'quotefault'
+#DB_NAME = 'quotefault'
+#DB_PASSWORD = ''
 # OpenID Connect SSO config
 OIDC_ISSUER = 'https://sso.csh.rit.edu/realms/csh'
 OIDC_CLIENT_CONFIG = {
