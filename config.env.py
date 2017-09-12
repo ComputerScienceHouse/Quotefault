ade9913c0a6de6caafd6e737a1f1698070d3813a
Devin Matte <devinmatte@gmail.com>
2017-09-12 18:19:07 -0400

    Plug can be configured in Config

--- config.env.py
+++ config.env.py
@@ -5,6 +5,7 @@ DEBUG = False
 IP = os.environ.get('QUOTEFAULT_IP', '127.0.0.1')
 PORT = os.environ.get('QUOTEFAULT_PORT', '8080')
 SERVER_NAME = os.environ.get('QUOTEFAULT_SERVER_NAME', 'quotefault.csh.rit.edu')
+PLUG = True
 #db info
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///{}'.format(os.path.join(os.getcwd(), "data.db")))
 #DB_HOST = os.environ.get('QUOTEFAULT_DB_HOST', 'mysql.csh.rit.edu')
